3a44465c82dd251bd1e7be62246502ec26a515fa
mykykh <49101849+mykykh@users.noreply.github.com>
2023-10-14 11:03:05 +0300

    initial commit

---
+++ init.lua
@@ -0,0 +1,14 @@
+vim.opt.ruler = true
+
+vim.opt.hlsearch = true
+
+vim.opt.smarttab = true
+vim.opt.autoindent = true
+
+-- tab as 4 spaces
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- show line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
